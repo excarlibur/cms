28d34d4c8ab058dbd0f48f73f4a81514dbfbfa1a
excarlibur <929090475@qq.com>
2017-04-26 10:29:11 +0800

    第一次提交

---
+++ cyt-common/src/main/java/com/cyt/CytCommonApplication.java
@@ -0,0 +1,12 @@
+package com.cyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CytCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CytCommonApplication.class, args);
+	}
+}
